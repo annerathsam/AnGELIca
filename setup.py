97de7fa7d6c14a0e8389c08312fc929d1bf47da0
Anne Rathsam <annerathsam@usp.br>
2026-06-01 18:17:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='AnGELIca',
     version='0.1.0',    
     description='Script to estimate ages for FGK stars based on their 3D NLTE lithium abundances, effective temperatures and [Fe/H].',
-    url='https://github.com/annerathsam/lithium_chronos',
+    url='https://github.com/annerathsam/AnGELIca',
     author='Anne Rathsam',
     author_email='annerathsam@usp.br',
     license='MIT',
-    packages=['lithium_chronos'],
+    packages=['AnGELIca'],
     install_requires=['numpy', 'pandas'])
